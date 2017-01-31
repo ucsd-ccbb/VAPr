64762e69760cb5fc6da58b2e369dbb3462021b6d
Mazzafish <carlo.mazzaferro@gmail.com>
2017-01-31 10:22:34 -0800

    Added automatic database download for annovar

--- VAPr/annovar_suprocess.py
+++ VAPr/annovar_suprocess.py
@@ -14,15 +14,17 @@ class AnnovarWrapper(object):
         self.output = output_csv + 'annotated'
         self.output_csv_path = output_csv
         self.path = annovar_path
-        self.down_dd = '-webfrom -annovar'
-        self.annovar_hosted = ['knownGene', 'esp6500siv2_all', '1000g2015aug_all' ]
+        self.down_dd = '-webfrom annovar'
+        self.annovar_hosted = ['knownGene', 'esp6500siv2_all', '1000g2015aug', 'nci60',
+                               'clinvar_20161128', 'popfreq_all_20150413', 'cosmic70']
+
         self.supported_databases = OrderedDict({'knownGene': 'g',
                                                 'tfbsConsSites': 'r',
                                                 'cytoBand': 'r',
                                                 'targetScanS': 'r',
                                                 'genomicSuperDups': 'r',
                                                 'esp6500siv2_all': 'f',
-                                                '1000g2015aug_all': 'f',
+                                                '1000g2015aug': 'f',
                                                 'popfreq_all_20150413': 'f',
                                                 'clinvar_20161128': 'f',
                                                 'cosmic70': 'f',
@@ -50,7 +52,6 @@ class AnnovarWrapper(object):
         return 'Finished running ANNOVAR on {}'.format(self.input)
 
     def build_db_dl_command_str(self, all, dbs):
-        #TODO: finalize automated database download
 
         if not all:
             databases = dbs
@@ -61,12 +62,11 @@ class AnnovarWrapper(object):
 
         for db in databases:
             if db in self.annovar_hosted:
-                command_list.append(" ".join(['perl', self.path + 'annotate_variation.pl', '-buildver', 'hg19', '-downb',
-                                    self.down_dd, db, 'humandb/']))
+                command_list.append(" ".join(['perl', self.path + 'annotate_variation.pl', '-buildver', 'hg19', '-downdb',
+                                    self.down_dd, db, self.path + 'humandb/']))
             else:
-                command_list.append(" ".join(['perl', self.path + 'annotate_variation.pl', '-buildver', 'hg19', '-downb',
+                command_list.append(" ".join(['perl', self.path + 'annotate_variation.pl', '-buildver', 'hg19', '-downdb',
                                               db, self.path + 'humandb/']))
-        print(command_list)
         return command_list
 
     def download_dbs(self, all=True, dbs=None):
@@ -75,9 +75,9 @@ class AnnovarWrapper(object):
         for command in list_commands:
             args = shlex.split(command)
             p = subprocess.Popen(args, stdout=subprocess.PIPE)
-            run_handler(self.path + 'humandb/')
+            run_handler(self.path + 'humandb/', download=True)
 
-        return 'Finished downloading databases tp'.format(self.path + 'humandb/')
+        return 'Finished downloading databases to {}'.format(self.path + 'humandb/')
 
 
 class MyHandler(FileSystemEventHandler):
@@ -86,25 +86,38 @@ class MyHandler(FileSystemEventHandler):
     and proceed to next file.
     """
 
-    def __init__(self, observer):
+    def __init__(self, observer, download=False):
+
         self.observer = observer
+        self.dl = download
 
     def on_created(self, event):
 
-        if event.src_path.split('.')[-1] not in ["invalid_input", "log"]:
-            print("Currently working on VCF file: " + event.src_path.split('/')[-1].split('.')[0] + ", field " +\
-                  event.src_path.split('/')[-1].split('.')[-1])
+        if self.dl:
+            if event.src_path.split('.')[-1] not in ["annovar_date"]:
+                print("Currently downloading database file: " + event.src_path.split('/')[-1].split('.')[0])
 
-            final = event.src_path[-3:]
-            if final == 'txt':
-                print("\nAnnovar finished working on file : " + event.src_path.split('/')[-1].split('.')[0] + \
-                      " has finished. A .txt file has been created in the OUT_PATH directory\n")
-                self.observer.stop()
+                final = event.src_path[-3:]
+                if final == 'txt':
+                    print("\nAnnovar finished working on file : " + event.src_path.split('/')[-1].split('.')[0] + \
+                          ". A .txt file has been created in the OUT_PATH directory\n")
+                    self.observer.stop()
+
+        else:
+            if event.src_path.split('.')[-1] not in ["invalid_input", "log"]:
+                print("Currently working on VCF file: " + event.src_path.split('/')[-1].split('.')[0] + ", field " +\
+                      event.src_path.split('/')[-1].split('.')[-1])
+
+                final = event.src_path[-3:]
+                if final == 'txt':
+                    print("\nAnnovar finished working on file : " + event.src_path.split('/')[-1].split('.')[0] +\
+                          ". A .txt file has been created in the OUT_PATH directory\n")
+                    self.observer.stop()
 
 
-def run_handler(output_csv_path):
+def run_handler(output_csv_path, download=False):
     observer = Observer()
-    event_handler = MyHandler(observer)
+    event_handler = MyHandler(observer, download=download)
     observer.schedule(event_handler, output_csv_path, recursive=True)
     observer.start()
     observer.join()
