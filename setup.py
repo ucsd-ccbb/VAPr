e96bc65bb99b04cf076e52134c39d2683e301502
Mazzafish <carlo.mazzaferro@gmail.com>
2017-01-31 10:21:48 -0800

    1st release

--- setup.py
+++ setup.py
@@ -1,16 +1,12 @@
 from setuptools import setup
 
 setup(name='VAPr',
-      version='0.1',
-      description='This package is aimed at providing a way of retrieving variant information using ANNOVAR '
-                  'http://annovar.openbioinformatics.org/en/latest/ and myvariant.info. In particular, it is'
-                  'suited for bioinformaticians interested in aggregating variant information into a single database.'
-                  'The data structure used is a python dictionaty, so the data can be easily parsed to a MongoDB'
-                  'instance.',
+      version='1.0',
+      description='Package for NoSQL variant data storage, annotation and prioritization.',
       url='https://github.com/ucsd-ccbb/VAPr',
       author='Carlo Mazzaferro',
       author_email='cmazzafe@ucsd.edu',
-      install_requires = ['pymongo', 'myvariant', 'pyvcf'],
+      install_requires = ['pymongo', 'myvariant', 'pyvcf', 'watchdog'],
       license='MIT',
       packages=['VAPr'],
       zip_safe=False)
