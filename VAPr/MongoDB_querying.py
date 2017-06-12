54e36a3ada4ac45801e501000dbe3c0fff132b20
carlomazzaferro <carlo.mazzaferro@gmail.com>
2017-06-12 16:46:45 -0700

    Updated docstrings

--- VAPr/MongoDB_querying.py
+++ VAPr/MongoDB_querying.py
@@ -6,6 +6,13 @@ class Filters(object):
     """
     Class for variant filtering. DB and collection names can be passed directly to the class and will be used in each
     method.
+
+    Description:
+
+        Rare variants filters are aimed at finding variants that occur with low frequency in the general population.
+        Further, some of the filters implemented also target variants whose gene function is known. Higher selectivity
+        filters such as the rare_high_impact_variant filter also screens variant data function using CADD Phred scores.
+
     """
 
     def __init__(self, db_name, collection_name):
@@ -14,15 +21,11 @@ class Filters(object):
         self.db_name = db_name
 
     def rare_cancer_variant(self):
-        """
-        Function for retrieving rare cancer variants.
-        :return:
-        """
+        """ Function for retrieving rare cancer variants """
 
         client = MongoClient()
         db = getattr(client, self.db_name)
         collection = getattr(db, self.collection_name)
-        #
         filtered = collection.find(
             {
                 "$and":
@@ -54,8 +57,9 @@ class Filters(object):
         return filtered
 
     def rare_disease_variant(self):
-        client = MongoClient()
+        """ Function for retrieving rare disease variants. Includes filter on Clinvar data  """
 
+        client = MongoClient()
         db = getattr(client, self.db_name)
         collection = getattr(db, self.collection_name)
 
@@ -96,7 +100,7 @@ class Filters(object):
         return filtered
 
     def rare_high_impact_variants(self):
-        """Rare high impact (CADD) variant filter"""
+        """ Function for retrieving rare disease variants. Includes filter on Clinvar as well CADD Phred data  """
 
         client = MongoClient()
 
@@ -141,6 +145,7 @@ class Filters(object):
         return filtered
 
     def deleterious_compound_heterozygote(self):
+        """ Function for retrieving deleterious compound heterozygote variants  """
 
         client = MongoClient()
 
@@ -162,10 +167,23 @@ class Filters(object):
         return filtered
 
     def get_de_novo(self, sample1, sample2, sample3, multisample=True):
+        """
+        Function for de novo variant analysis. Can be performed on multisample files or or on data coming
+        from a collection of files. In the former case, every sample contains the same variants, although they have
+        differences in their allele frequency and read values. A de novo variant is defined as a variant that
+        occurs only in the specified sample (sample1) and not on the other two (sample2, sample3). Occurrence is
+        defined as having allele frequencies greater than [0, 0] ([REF, ALT]).
+
+        For the case in which the variants come from multiple files, de novo variants are the ones that occur only
+        once on the sample specified (usually found in one file), and that do not occur on the other samples (found
+        in the other files).
+        """
+
         client = MongoClient()
         db = getattr(client, self.db_name)
         collection = getattr(db, self.collection_name)
         if multisample:
+
             filtered_hgvs = collection.distinct("hgvs_id",
                                                 {
                                                     "$or": [
@@ -207,7 +225,7 @@ class Filters(object):
                             "if": {
                                 "$and": [
                                     {"$in": [sample1, "$samples"]},
-                                    {"$eq": [{"$size": "$samples"}, 1]}
+                                    {"$eq": [{"$size": "$samples"}, 1]}  # Occurs only once, hence a unique HGVS_ID
                                 ]
                             },
                             "then": "$$KEEP",
@@ -220,5 +238,5 @@ class Filters(object):
             as_hgvs_list = [i['_id'] for i in filtered_hgvs]
             de_novo = collection.find({'hgvs_id': {'$in': as_hgvs_list}})
 
-        print('Variants found that match rarity criteria: {}'.format(len(de_novo)))
+        print('Variants found that match de novo criteria: {}'.format(len(de_novo)))
         return list(de_novo)
