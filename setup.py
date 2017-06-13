3f6ec55d99d35a82ded91111c61a4c4cc78e5b21
carlomazzaferro <carlo.mazzaferro@gmail.com>
2017-06-13 16:46:53 -0700

    New releases with fixes

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='VAPr',
-      version='2.0',
+      version='2.0.2',
       description='Package for NoSQL variant data storage, annotation and prioritization.',
       url='https://github.com/ucsd-ccbb/VAPr',
       author='Carlo Mazzaferro',
       author_email='cmazzafe@ucsd.edu',
-      install_requires=['pymongo', 'myvariant', 'pyvcf', 'watchdog', 'pandas', 'pathos'],
+      install_requires=['pymongo', 'myvariant', 'pyvcf', 'pandas', 'tqdm'],
       license='MIT',
       packages=['VAPr'],
       zip_safe=False)
