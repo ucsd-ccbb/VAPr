5f9d688e49520822f9f4e5b68d03f1ccd7120947
carlomazzaferro <carlo.mazzaferro@gmail.com>
2017-06-20 22:57:04 -0700

    Refactored module’s name

---
+++ VAPr/queries.py
@@ -0,0 +1,281 @@
+from pymongo import MongoClient
+
+
+class Filters(object):
+
+    """
+    Class for variant filtering. DB and collection names can be passed directly to the class and will be used in each
+    method.
+
+    Description:
+
+        Rare variants filters are aimed at finding variants that occur with low frequency in the general population.
+        Further, some of the filters implemented also target variants whose gene function is known. Higher selectivity
+        filters such as the rare_high_impact_variant filter also screens variant data function using CADD Phred scores.
+
+    """
+
+    def __init__(self, db_name, collection_name):
+
+        self.collection_name = collection_name
+        self.db_name = db_name
+
+    def variants_from_sample(self, sample_name):
+
+        client = MongoClient()
+        db = getattr(client, self.db_name)
+        collection = getattr(db, self.collection_name)
+        filtered = collection.find({'sample_id': sample_name})
+        return list(filtered)
+
+    def rare_cancer_variant(self, samples):
+        """ Function for retrieving rare cancer variants """
+
+        client = MongoClient()
+        db = getattr(client, self.db_name)
+        collection = getattr(db, self.collection_name)
+        if not samples:
+            samples = collection.distinct('sample_id')
+
+        filtered = collection.find(
+            {
+                "$and":
+                    [
+                        {
+                            "$or":
+                                [
+                                    {"esp6500siv2_all": {"$lt": 0.05}},
+                                    {"esp6500siv2_all": {"$exists": False}}
+                                ]
+                        },
+                        {
+                            "$or":
+                                [
+                                    {"func_knowngene": "exonic"},
+                                    {"func_knowngene": "splicing"}
+                                ]
+                        },
+                        {"exonicfunc_knowngene": {"$ne": "synonymous SNV"}},
+                        {"filter_passing_reads_count": {"$gte": 10}},
+                        {"cosmic70": {"$exists": True}},
+                        {"1000g2015aug_all": {"$lt": 100}},
+                        {"sample_id": {"$in": samples}}
+                    ]
+            }
+        )
+
+        filtered = list(filtered)
+        print('Variants found that match rarity criteria: {}'.format(len(filtered)))
+        return filtered
+
+    def rare_disease_variant(self, samples=None):
+        """ Function for retrieving rare disease variants. Includes filter on Clinvar data  """
+
+        client = MongoClient()
+        db = getattr(client, self.db_name)
+        collection = getattr(db, self.collection_name)
+        if not samples:
+            samples = collection.distinct('sample_id')
+
+        filtered = collection.find(
+            {
+                "$and":
+                    [
+                        {
+                            "$or":
+                                [
+                                   {"esp6500siv2_all": {"$lt": 0.05}},
+                                   {"esp6500siv2_all": {"$exists": False}}
+                                ]
+                        },
+                        {
+                            "$or":
+                                [
+                                    {"func_knowngene": "exonic"},
+                                    {"func_knowngene": "splicing"}
+                                ]
+                        },
+                        {"exonicfunc_knowngene": {"$ne": "synonymous SNV"}},
+                        {"filter_passing_reads_count": {"$gte": 10}},
+                        {
+                            "$or":
+                                [
+                                    {"cosmic70": {"$exists": True}},
+                                    {"clinvar": {"$exists": True}}
+                                ]
+                        },
+                        {"1000g2015aug_all": {"$lt": 0.1}},
+                        {"sample_id": {"$in": samples}},
+                    ]
+            }
+        )
+
+        filtered = list(filtered)
+        print ('Variants found that match rarity criteria: {}'.format(len(filtered)))
+        return filtered
+
+    def rare_high_impact_variants(self, samples=None):
+        """ Function for retrieving rare disease variants. Includes filter on Clinvar as well CADD Phred data  """
+
+        client = MongoClient()
+
+        db = getattr(client, self.db_name)
+        collection = getattr(db, self.collection_name)
+        if not samples:
+            samples = collection.distinct('sample_id')
+
+        filtered = collection.find(
+            {
+                "$and":
+                    [
+                        {
+                            "$or":
+                                [
+                                    {"esp6500siv2_all": {"$lt": 0.05}},
+                                    {"esp6500siv2_all": {"$exists": False}}
+                                ]
+                        },
+                        {
+                            "$or":
+                                [
+                                    {"func_knowngene": "exonic"},
+                                    {"func_knowngene": "splicing"}
+                                ]
+                        },
+                        {"exonicfunc_knowngene": {"$ne": "synonymous SNV"}},
+                        {"filter_passing_reads_count": {"$gte": 10}},
+                        {
+                            "$or":
+                                [
+                                    {"cosmic70": {"$exists": True}},
+                                    {"clinvar": {"$exists": True}}
+                                ]
+                        },
+                        {"1000g2015aug_all": {"$lt": 0.1}},
+                        {"cadd.phred": {"$gte": 15}},
+                        {"sample_id": {"$in": samples}},
+                    ]
+            }
+        )
+
+        filtered = list(filtered)
+        print('Variants found that match rarity criteria: {}'.format(len(filtered)))
+        return filtered
+
+    def deleterious_compound_heterozygote(self, samples=None):
+        """ Function for retrieving deleterious compound heterozygote variants  """
+
+        client = MongoClient()
+
+        db = getattr(client, self.db_name)
+        collection = getattr(db, self.collection_name)
+        if not samples:
+            samples = collection.distinct('sample_id')
+
+        filtered = collection.find(
+            {
+                "$and":
+                    [
+                        {"genotype_subclass_by_class.heterozygous": "compound"},
+                        {"cadd.phred": {"$gte": 10}},
+                        {"sample_id": {"$in": samples}},
+                    ]
+            }
+        )
+
+        filtered = list(filtered)
+        print('Variants found that match rarity criteria: {}'.format(len(filtered)))
+        return filtered
+
+    def get_de_novo(self, sample1, sample2, sample3, multisample=True):
+        """
+        Function for de novo variant analysis. Can be performed on multisample files or or on data coming
+        from a collection of files. In the former case, every sample contains the same variants, although they have
+        differences in their allele frequency and read values. A de novo variant is defined as a variant that
+        occurs only in the specified sample (sample1) and not on the other two (sample2, sample3). Occurrence is
+        defined as having allele frequencies greater than [0, 0] ([REF, ALT]).
+
+        For the case in which the variants come from multiple files, de novo variants are the ones that occur only
+        once on the sample specified (usually found in one file), and that do not occur on the other samples (found
+        in the other files).
+        """
+
+        client = MongoClient()
+        db = getattr(client, self.db_name)
+        collection = getattr(db, self.collection_name)
+        if multisample:
+
+            filtered_hgvs = collection.distinct("hgvs_id",
+                                                {
+                                                    "$or": [
+                                                            {"$and": [
+                                                                {"alleles": [0, 0]},
+                                                                {"sample_id": sample2}
+                                                            ]
+                                                            },
+                                                            {"$and": [
+                                                                {"alleles": [0, 0]},
+                                                                {"sample_id": sample3}
+                                                            ]
+                                                            },
+                                                        ]
+                                                }
+                                                )
+
+            de_novo = collection.find(
+                {
+                    "$and":
+                        [
+                            {"hgvs_id": {"$in": filtered_hgvs}},
+                            {"alleles": {"$ne": [0, 0]}},
+                            {"sample_id": sample1}
+                        ]
+
+                }
+            )
+        else:
+            filtered_hgvs = collection.aggregate(
+                [
+                    {"$group": {
+                        "_id": "$hgvs_id",
+                        "samples":
+                            {"$addToSet": {
+                                "$cond": [  # works with a if ... then ... else syntax
+
+                                    {  # if these are satisfied,
+                                        "$or": [
+                                            {"$eq": ["$sample_id", sample1]},
+                                            {"$eq": ["$sample_id", sample2]},
+                                            {"$eq": ["$sample_id", sample3]}
+                                        ]
+                                    },
+                                    # then add sample_id to the set of variants
+                                    "$sample_id",
+                                    # else, add zero
+                                    0
+                                ]
+
+                            }
+                            }
+                    }
+                    },
+                    {"$redact": {
+                        "$cond": {
+                            "if": {
+                                "$and": [
+                                    {"$in": [sample1, "$samples"]},
+                                    {"$eq": [{"$size": "$samples"}, 1]}  # Occurs only once, hence a unique HGVS_ID
+                                ]
+                            },
+                            "then": "$$KEEP",
+                            "else": "$$PRUNE"
+                        }
+                    }
+                    }
+                ]
+            )
+            as_hgvs_list = [i['_id'] for i in filtered_hgvs]
+            de_novo = collection.find({'hgvs_id': {'$in': as_hgvs_list}})
+
+        print('Variants found that match de novo criteria: {}'.format(len(de_novo)))
+        return list(de_novo)
---
+++ VAPr/writes.py
@@ -0,0 +1,119 @@
+import pandas
+import vcf
+from pymongo import MongoClient
+
+
+class Writer(object):
+
+    def __init__(self, db_name, collection_name):
+
+        self.collection_name = collection_name
+        self.db_name = db_name
+        self.client = MongoClient()
+        self.db = getattr(self.client, self.db_name)
+        self.collection = getattr(self.db, self.collection_name)
+
+    def generate_unfiltered_annotated_csv(self, filepath):
+        """
+        :param filepath: filpath (including name of output file) to which the output will be written
+        :return: annotated csv file
+        """
+        all_my_data = list(self.collection.find({}))
+        df = pandas.DataFrame(all_my_data)
+
+        with open(filepath, "w") as f:
+            df.to_csv(f)
+
+        return 'Finished writing annotated CSV file'
+
+    def generate_unfiltered_annotated_vcf(self, vcf_input_path, vcf_output_path, info_out=True):
+        """
+        :param vcf_input_path: template vcf file (inital vcf from which a new one will be created)
+        :param vcf_output_path: name and filepath to where new vcf file will be written
+        :param info_out: if set to true (Default), will write all annotation data to INFO column, else, it won't.
+        :return: writed vcf file to specified directory
+        """
+
+        all_my_data = list(self.collection.find({}))
+        chr_vars = []
+        location_vars_ant = []
+        location_vars_pos = []
+
+        for i in range(0, len(all_my_data)):
+            if all_my_data[i]['chr'] == 'chrMT':
+                chr_vars.append('chrM')
+            else:
+                chr_vars.append(all_my_data[i]['chr'])
+            location_vars_ant.append(all_my_data[i]['start'] + 1)
+            location_vars_pos.append(all_my_data[i]['start'] - 1)
+
+        vcf_reader = vcf.Reader(filename=vcf_input_path)
+        vcf_writer = vcf.Writer(open(vcf_output_path, 'w'), vcf_reader)
+
+        for i in range(0, len(chr_vars)):
+            for record in vcf_reader.fetch(chr_vars[i], location_vars_pos[i], location_vars_ant[i]):
+                if info_out is True:
+                    record.INFO.update(all_my_data[i])
+                    vcf_writer.write_record(record)
+                else:
+                    vcf_writer.write_record(record)
+
+        return 'Finished writing annotated VCF file'
+
+    @staticmethod
+    def generate_annotated_csv(list_dictionaries, filepath):
+        """
+        :param list_dictionaries: list of annotated variants in dictionary
+        :param filepath: filpath (including name of output file) to which the output will be written
+        :return: annotated csv file
+        """
+        df = pandas.DataFrame(list_dictionaries)
+        with open(filepath, "w") as f:
+            df.to_csv(f)
+
+        return 'Finished writing annotated, filtered CSV file'
+
+    @staticmethod
+    def generate_annotated_vcf(joint_list, vcf_input_path, vcf_output_path, info_out=True):
+        """
+        :param vcf_input_path: template vcf file (initial vcf from which a new one will be created)
+        :param vcf_output_path: name and filepath to where new vcf file will be written
+        :param joint_list: list of dictionaties contatining annotations from ANNOVAR & myvariant
+        :param info_out: if set to true (Default), will write all annotation data to INFO column, else, it won't.
+
+        :return: None
+        """
+        chr_vars = []
+        location_vars_ant = []
+        location_vars_pos = []
+
+        for i in range(0, len(joint_list)):
+            if joint_list[i]['chr'] == 'chrMT':
+                chr_vars.append('chrM')
+            else:
+                chr_vars.append(joint_list[i]['chr'])
+            location_vars_ant.append(joint_list[i]['start'] + 1)
+            location_vars_pos.append(joint_list[i]['start'] - 1)
+
+        vcf_reader = vcf.Reader(filename=vcf_input_path)
+        vcf_writer = vcf.Writer(open(vcf_output_path, 'w'), vcf_reader)
+
+        for i in range(0, len(chr_vars)):
+            for record in vcf_reader.fetch(chr_vars[i], location_vars_pos[i], location_vars_ant[i]):
+                if info_out is True:
+                    record.INFO.update(joint_list[i])
+                    vcf_writer.write_record(record)
+                else:
+                    vcf_writer.write_record(record)
+
+        return 'Finished writing annotated, filtered VCF file'
+
+    def generate_pandas_df(self):
+
+        client = MongoClient()
+        db = getattr(client, self.db_name)
+        collection = getattr(db, self.collection_name)
+        all_my_data = list(collection.find({}))
+
+        df = pandas.DataFrame(all_my_data)
+        return df
