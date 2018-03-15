f42c197385916a5e50dd1a2ffea4674150b8f463
Carlo Mazzaferro <carlo.mazzaferro@gmail.com>
2018-03-15 01:36:16 -0400

    sphinx minimal version

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ release = '3.0'
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-# needs_sphinx = '1.0'
+needs_sphinx = '1.7.1'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
